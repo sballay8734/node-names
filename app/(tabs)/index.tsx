40c76ddaace1ba26df4bb47f9f35e9cd54ca93af
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-06-12 19:52:17 -0400

    reset: Project reset before gestures course

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,57 +1,12 @@
-import React, { useState } from "react";
-import { StyleSheet, Pressable, Text, View } from "react-native";
-import * as d3 from "d3";
-import { generateDataset } from "@/data/testDataset";
-import { useSpring } from "@react-spring/native";
-import TestForceGraph from "@/components/ChartElements/TestForceGraph";
-import mainMockData from "../../data/mainMockData.json";
-import { useSharedValue } from "react-native-reanimated";
-import { Canvas, Circle } from "@shopify/react-native-skia";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 type Data = number[][];
 
 const Index = () => {
-  // TODO: This just triggers a reload of TestForceGraph
-  const [dataset, setDataset] = useState<Data>(generateDataset());
-
-  // **************************** YESSSSSSSSS *********************************
-  const props = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: { duration: 3000 },
-  });
-
-  const radius = useSharedValue(20);
-  const x = 100;
-  const y = 100;
-  const diameter = radius.value * 2;
-
   return (
     <View style={styles.container}>
-      <Pressable
-        style={{
-          height: diameter,
-          width: diameter,
-        }}
-        onPress={() => (radius.value += 10)}
-      >
-        <Canvas
-          style={{
-            height: diameter,
-            width: diameter,
-            backgroundColor: "purple",
-          }}
-        >
-          <Circle cx={diameter / 2} cy={diameter / 2} r={radius} color="red" />
-        </Canvas>
-      </Pressable>
-      {/* <TestForceGraph dataset={mainMockData} />} */}
-      <Pressable
-        style={styles.button}
-        onPress={() => setDataset(generateDataset())}
-      >
-        <Text style={styles.text}>Generate New Data</Text>
-      </Pressable>
+      <Text style={{ color: "white" }}>Hello World</Text>
     </View>
   );
 };
@@ -63,27 +18,7 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#452b2b",
-  },
-  button: {
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-    bottom: 0,
-    marginBottom: 10,
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: "black",
-    opacity: 0.9,
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
+    backgroundColor: "#212121",
   },
 });
 
