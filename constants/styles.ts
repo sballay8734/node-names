27316a18783ad0fde14a1ac8c6d4dc3b3bf5eacc
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-07-25 06:22:29 -0400

    chore(todos): brainstorming

--- constants/styles.ts
+++ constants/styles.ts
@@ -3,3 +3,12 @@ export const ARROW_BTN_RADIUS = 25;
 // REVIEW: changing TAB_BAR_HEIGHT will NOT effect ACTUAL tab bar height
 // REVIEW: It was hard-coded (after inspection) to calculate arrow position
 export const TAB_BAR_HEIGHT = 79;
+
+// BELOW IS JUST BRAINSTORMING
+// You'll need to track focused item or current center position idk
+export const DEFAULT_SCALE = 0.4;
+
+export const FOCUS_SCALE = 1;
+
+// DON'T RENDER THESE NODES UNTIL USER ACTUALLY "Inspects"
+export const INSPECT_SCALE = 2;
